6400e6944b128050fe1cdd887257392c94a85b42
Johan Degn <degnjohan@gmail.com>
2024-11-05 11:23:06 +0100

    Added word size optimization for XORing seeds into v

--- vole.c
+++ vole.c
@@ -189,19 +189,20 @@ void partial_vole_commit(const uint8_t* rootKey, const uint8_t* iv, unsigned int
   prg(rootKey, iv, expanded_keys, lambda, lambda_bytes * tau);
 
   unsigned int col_idx = 0;
+  // Iterate over each tree
   for (unsigned int t = 0; t < tau; t++) {
     unsigned int depth = t < tau0 ? k0 : k1;
+    unsigned int num_instances = 1 << depth;
 
     vector_commitment(expanded_keys + t * lambda_bytes, lambda, depth, path, &vec_com);
 
-    const unsigned int num_instances = 1 << depth;
+    // Iterate each seed emmited from the tree
     for (unsigned int i = 0; i < num_instances; i++) {
       extract_sd_com(&vec_com, iv, lambda, i, sd, com);
       // TODO: only compute necessary part of r
       prg(sd, iv, r, lambda, ellhat_bytes);
 
-      // Extract start to end bits from r
-      // For-loop going over bytes that needs to be copied from r to r_trunc
+      // Extract and align the requested part of r
       unsigned int bit_offset = start % 8;
       unsigned int start_byte = start / 8;
       for (unsigned int j = 0; j < len_bytes - 1; j++) {
@@ -209,20 +210,27 @@ void partial_vole_commit(const uint8_t* rootKey, const uint8_t* iv, unsigned int
       }
       // Apply last byte
       r_trunc[len_bytes - 1] = (r[start_byte + len_bytes - 1] << bit_offset) & 0xFF << bit_offset;
-      // XOR the truncated r with the v
 
-      // Iterate depth and XOR into correct entries
+      // XOR directly into v instead of maintaining a stack to save memory
       for (unsigned int j = 0; j < depth; j++) {
+        // Only apply to correct entries
         if ((i >> j) & 1) {
-          // Shift offset and xor into v
+          // Shift offset and XOR into v
           unsigned int row_bit_index = len * (col_idx + j);
           unsigned int row_bit_offset = row_bit_index % 8;
+          // Apply first byte
           v[row_bit_index / 8] ^= r_trunc[0] >> row_bit_offset;
-          // TODO: Use xor_u32_array most of the way
-          for (unsigned int k = 1; k < len_bytes; k++) {
+          unsigned int k = 1;
+          // Apply in words sized chunks
+          for (; k < len_bytes - sizeof(size_t); k += sizeof(size_t)) {
+            v[row_bit_index / 8 + k] ^= r_trunc[k - 1] << (8 - row_bit_offset);
+            *(size_t *)(v + row_bit_index / 8 + k) ^= *((size_t *)(r_trunc + k)) >> row_bit_offset;
+          }
+          // Apply the remaining
+          for (; k < len_bytes; k++) {
             v[row_bit_index / 8 + k] ^= r_trunc[k - 1] << (8 - row_bit_offset) | r_trunc[k] >> row_bit_offset;
           }
-          // Avoid writing over byte boundery of v
+          // Avoid writing last byte over boundery of v if aligend
           if (row_bit_offset != 0) {
             v[row_bit_index / 8 + len_bytes] ^= r_trunc[len_bytes - 1] << (8 - row_bit_offset);
           }
